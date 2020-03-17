c05744e25655c7273bf72fe2f4ff9ca81a13d9ec
Travis Eiler <travis.l.eiler@gmail.com>
2020-03-17 10:53:00 -0700

    CPW 245 Winter 2020 Homework Assignment 9

--- Assign09/countNegativesTester.java
+++ Assign09/countNegativesTester.java
@@ -1,20 +1,22 @@
 // Travis Eiler
 // CPW 245, Winter '20
 // Assignment #9: Functional Programming
-// Due: Jan 22, 2020
+// Due: Mar 18, 2020
 // 
 // The purpose of this program is to test the method countNegatives().
 // 
 // The purpose of this assignment is to use stream operations to create the method
 // countNegatives() to count how many negative integers are in a given array.
 
+
 // I could have used the catch all import statement ( import java.util.*; ),
 // but I wanted to be specific about which classes/packages are required to 
 // run this program.
 import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.stream.*;
 
-public class countNegativesTester{
+
+public class CountNegativesTester{
     public static void main( String[] args ) {
         
         int[] arr01 =  {5, -1, -3, 20, 47, -10, -8, -4, 0, -6, -6};
@@ -30,10 +32,12 @@ public class countNegativesTester{
         System.out.println( countNegatives( arr04 ));
     }
     
+    
     public static long countNegatives( int[] arr ) {
         // https://docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html
         // Stream method:  Type:  Description:
         // count()         long   returns the count of elements in a stream
+        
         return Arrays.stream(arr).filter(n -> n < 0).count();
     }
 }
